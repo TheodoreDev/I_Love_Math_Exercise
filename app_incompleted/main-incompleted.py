79b2c01452685bf639955e95ccd0015d91e52226
Tom Huynh <theodore.huynh.melb@gmail.com>
2020-01-08 11:25:39 +0700

    update exercise

--- app_incompleted/main-incompleted.py
+++ app_incompleted/main-incompleted.py
@@ -16,56 +16,22 @@ app = Flask(__name__)
 app.secret_key = b'A Super Secret Key'
 
 
+# YOUR PART: change to your new model h5 and add more label_names like how you train in your notebook
 model = tf.keras.models.load_model('static/first_half_mul.h5')
 label_names = ['0', '1', '2', '3', '4', '5', '10']
 
-
-
 app.register_blueprint(home_page)
-
-
 
 def parse_image(imgData):
     imgstr = re.search(b"base64,(.*)", imgData).group(1)
     img_decode = base64.decodebytes(imgstr)
     with open("output.jpg", "wb") as file:
         file.write(img_decode)
-    # print('test1')
     return img_decode
 
-def deskew(image, width):
-    (h, w) = image.shape[:2]
-    moments = cv2.moments(image)
+# YOUR PART: deskew function
 
-    skew = moments['mu11'] / moments['mu02']
-    M = np.float32([[1, skew, -0.5*w*skew],
-                    [0, 1, 0]])
-    image = cv2.warpAffine(image, M, (w, h), flags=cv2.WARP_INVERSE_MAP | cv2.INTER_LINEAR)
-
-    image = imutils.resize(image, width=width)
-
-    return image
-
-def center_extent(image, size):
-    (eW, eH) = size
-
-    if image.shape[1] > image.shape[0]:
-        image = imutils.resize(image, width=eW)
-    else:
-        image = imutils.resize(image, height=eH)
-
-    extent = np.zeros((eH, eW), dtype='uint8')
-    offsetX = (eW - image.shape[1]) // 2
-    offsetY = (eH - image.shape[0]) // 2
-    extent[offsetY:offsetY + image.shape[0], offsetX:offsetX+image.shape[1]] = image
-
-    CM = mahotas.center_of_mass(extent)
-    (cY, cX) = np.round(CM).astype("int32")
-    (dX, dY) = ((size[0]//2) - cX, (size[1] // 2) - cY)
-    M = np.float32([[1, 0, dX], [0, 1, dY]])
-    extent = cv2.warpAffine(extent, M, size)
-
-    return extent
+# YOUR PART: center_extent function
 
 @app.route("/upload/", methods=["POST"])
 def upload_file():
@@ -73,55 +39,66 @@ def upload_file():
     nparr = np.fromstring(img_raw, np.uint8)
     image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # YOUR PART: convert to gray
 
-    blurred = cv2.GaussianBlur(gray, (5,5), 0)
-    edged = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 4)
+    gray = 
 
-    _, cnts, _ =  cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # YOUR PART: blur with GaussianBlur (play around with parameters)
+    blurred = 
+
+    # YOUR PART: edge with adaptiveThreshold (play around with parameters)
+    edged =
+
+    # YOUR PART: find contours, remember to input with edged.copy()
+    _, cnts, _ =    
+
+    # sort based on (x,y) position
     cnts = sorted([(c, cv2.boundingRect(c)[0]) for c in  cnts], key=lambda x: x[1])
 
     math_detect = []
 
     for (c, _) in cnts:
-        (x, y, w, h) = cv2.boundingRect(c)
+
+        # find rectangle box of a coutour
+        (x, y, w, h) = 
         if w >=5 and h>5:
+
+            # make sure we cut it a bit longer in height
             roi = edged[y:y+int(1.2*h), x:x+w]
             thresh = roi.copy()
 
-            thresh = deskew(thresh, 28)
-            thresh = center_extent(thresh, (28, 28))
+            # YOUR PART: deskew now with size 28
+            thresh = 
+
+            # YOUR PART: center_extent now with size (28, 28)
+            thresh = 
+
+            # to be sure, reshape to correct dimension 
             thresh = np.reshape(thresh, (28, 28, 1))
-            thresh = thresh / 255
+
+            # standardization by divide it by 255
+            thresh
+            
+            # make prediction 
             predictions = model.predict(np.expand_dims(thresh, axis=0))
             digit = np.argmax(predictions[0])
+
+            # draw rectangle on image (for debugging purpose - optional)
             cv2.rectangle(image, (x,y), (x+w, y+h), (0,255,0), 2)
+
+            # draw text on image (for debugging purpose - optional)
             cv2.putText(image, label_names[digit], (x-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 2)
 
-            if label_names[digit] == "1":
-                countt = 0
-                mem = []
-                for i in range(len(thresh[9])):
-                    if thresh[9][i] > 0: 
-                        countt +=1
-                        mem.append(i)
-                if countt >= 3 :
-                        math_detect.append("1")
-                else:
-                    math_detect.append("/")
 
-            else: 
-                math_detect.append(label_names[digit])
+            # LATER: when we want to add other operations like division, we need to add more condition below (some hacks)
+            math_detect.append(label_names[digit])
 
     def convert_math(math_detect):
         for i in range(0, len(math_detect)):
 
             if math_detect[i] == '10':
                 math_detect[i] = '*'
-            elif math_detect[i] == '11':
-                math_detect[i] = '-'
-            elif math_detect[i] == '12':
-                math_detect[i] = '+'
+            # YOUR PART: add more condition for 11 and 12 for minus and plus
            
         return math_detect
 
@@ -143,6 +120,7 @@ def calcu():
     val = str(request.get_data())
     val1=val[2:-1]
 
+    # eval allows use to calculate the string of math "4+5", "6*4+2", "4/6+52"
     result = str(eval(val1))
     return result
 
